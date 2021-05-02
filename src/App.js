bf7ab71ef55f08484f5a6ca492b337bf80b0dc84
Robin Bette <robinbette@hotmail.com>
2021-05-02 15:06:30 +0000

    Lift the state to a parent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
-import React, { Component } from 'react';
-import NavBar from './components/navbar';
-import Counters from './components/counters';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
 
 class App extends Component {
-    state = {  }
-    render() { 
-        return (
-        <React.Fragment>
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    // We do not update the state directly.
+    // Instead, create new counters array and give that to the set state method and have React update the state.
+
+    // Use the spread operator (...) to be able to clone the exact array
+    const counters = [...this.state.counters];
+
+    // However, this seems to work exactly like that as
+    // const counters = this.state.counters;
+
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+
+    // this.setState({ counters: counters }); as key and value are equal, simplify like:
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
         <NavBar />
         <main className="container">
-            <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
-        </React.Fragment>
-        );
-    }
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,56 +2,19 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    // We do not update the state directly.
-    // Instead, create new counters array and give that to the set state method and have React update the state.
-
-    // Use the spread operator (...) to be able to clone the exact array
-    const counters = [...this.state.counters];
-
-    // However, this seems to work exactly like that as
-    // const counters = this.state.counters;
-
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-
-    // this.setState({ counters: counters }); as key and value are equal, simplify like:
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">
+        <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.state.counters.map((counter) => (
-          <Counter key={counter.id} onIncrement={this.handleIncrement} onDelete={this.handleDelete} counter={counter} />
+        {this.props.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onIncrement={this.props.onIncrement}
+            onDelete={this.props.onDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );

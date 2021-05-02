df6c6c95f3448298a28c6c3fad4f0833c81becf3
Robin Bette <robinbette@hotmail.com>
2021-05-02 14:09:46 +0000

    Add reset button and handleReset method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   // Bind event handlers via constructor + super and method.bind(this),
@@ -17,6 +17,9 @@ class Counter extends Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
           Increment
+        </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">
+          Delete
         </button>
       </div>
     );

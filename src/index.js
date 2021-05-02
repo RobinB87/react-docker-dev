edf24d990cf3f450187c7160a69799c84fbbfffe
Robin Bette <robinbette@hotmail.com>
2021-05-02 14:58:28 +0000

    Implement App.js as the parent

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Counters from './components/counters';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 
 ReactDOM.render(
-  <Counters />,
+  <App />,
   document.getElementById('root')
 );
 

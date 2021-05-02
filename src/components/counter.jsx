8f4744c2d72101cd1b1cd273c1d1059d3b8cbf2b
Robin Bette <robinbette@hotmail.com>
2021-05-02 11:53:51 +0000

    Update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,15 +8,21 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './components/counter';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter />,
   document.getElementById('root')
 );
 
